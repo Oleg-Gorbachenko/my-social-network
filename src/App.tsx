fcf01b6d9831d004a6981ac843e22f30e2334390
Oleg <viktorovich959595@mail.ru>
2022-02-03 12:45:06 +1000

    delete header,footer and technologies. Create new social network

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,57 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./Components/Header/Header";
-import {Technologies} from "./Components/Technologies/Technologies";
-import {Footer} from "./Components/Footer/Footer";
 
 function App() {
-  return (
-      <div>
-        <Header/>
-        <Technologies/>
-          <Footer/>
-      </div>
-  );
+    return (
+        <div className="app-wrapper">
+            <header className="header">
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSZ9se8N-sEQ-LU7cYhO9hWVljFF3eS1vUYQ&usqp=CAU"
+                    alt="header-img"/>
+            </header>
+            <nav className="nav">
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className="content">
+                <div>
+                    <img src="https://klike.net/uploads/posts/2019-05/1556708032_1.jpg"
+                         alt="content-img"/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
 }
 
 export default App;
