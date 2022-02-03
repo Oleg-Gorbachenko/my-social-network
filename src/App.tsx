bcc0fe49e3beda955c486ec66d16e0e7adc07fcc
Oleg <viktorovich959595@mail.ru>
2022-02-03 12:09:47 +1000

    create Footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import {Header} from "./Components/Header/Header";
 import {Technologies} from "./Components/Technologies/Technologies";
+import {Footer} from "./Components/Footer/Footer";
 
 function App() {
   return (
       <div>
         <Header/>
         <Technologies/>
+          <Footer/>
       </div>
   );
 }
