2a58fe6c4ad34f72cb2b477a1e54b4cf616a44fe
Oleg <viktorovich959595@mail.ru>
2022-02-04 17:58:54 +1000

    typed dialogs

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,30 +1,47 @@
 import React from "react";
 import classes from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
+type DialogItemType = {
+    name: string
+    id: number
+}
+
+type MessageType = {
+    message: string
+}
+
+export const DialogItem = (props: DialogItemType) => {
+    return (
+        <div className={classes.dialog + " " + classes.active}>
+            <NavLink to={"/dialogs/" + props.id}>{props.name}</NavLink>
+        </div>
+    )
+}
+
+export const Message = (props: MessageType) => {
+    return (
+        <div className={classes.message}>{props.message}</div>
+    )
+}
 
 export const Dialogs = () => {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <div className={classes.dialog + " " + classes.active}>
-                    Andrey
-                </div>
-                <div className={classes.dialog}>
-                    Roma
-                </div>
-                <div className={classes.dialog}>
-                    Viktor
-                </div>
-                <div className={classes.dialog}>
-                    Igor
-                </div>
+                <DialogItem name={'Andrey'} id={1}/>
+                <DialogItem name={'Oleg'} id={2}/>
+                <DialogItem name={'Maks'} id={3}/>
+                <DialogItem name={'Anton'} id={4}/>
+                <DialogItem name={'Viktoriya'} id={5}/>
+                <DialogItem name={'Dmitry'} id={6}/>
+
             </div>
             <div className={classes.messages}>
-                <div className={classes.message}>Hi</div>
-                <div className={classes.message}>How are you</div>
-                <div className={classes.message}>Yo</div>
-
-
+                <Message message={'Hi!'}/>
+                <Message message={'How are you'}/>
+                <Message message={'Yo'}/>
+                <Message message={'Abrakadabra'}/>
             </div>
         </div>
     )
