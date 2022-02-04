af31acc47c10eeb85731df9aedc1d67ddfcfe019
Oleg <viktorovich959595@mail.ru>
2022-02-04 17:14:08 +1000

    combed styles

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -3,9 +3,29 @@ import classes from './Dialogs.module.css'
 
 
 export const Dialogs = () => {
-  return (
-      <div>
-          Dialogs
-      </div>
-  )
+    return (
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + " " + classes.active}>
+                    Andrey
+                </div>
+                <div className={classes.dialog}>
+                    Roma
+                </div>
+                <div className={classes.dialog}>
+                    Viktor
+                </div>
+                <div className={classes.dialog}>
+                    Igor
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>How are you</div>
+                <div className={classes.message}>Yo</div>
+
+
+            </div>
+        </div>
+    )
 }
