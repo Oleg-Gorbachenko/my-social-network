b6811b43cf0d0688f061fd8922174a8c11853786
Oleg <viktorovich959595@mail.ru>
2022-05-11 09:17:06 +1000

    Updates

---
+++ precache-manifest.62faf30388434eb9c2f80e205029de94.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cddeed9529cbdab009cc0b22b2e640d",
+    "url": "/my-social-network/index.html"
+  },
+  {
+    "revision": "7513caea159afe168f51",
+    "url": "/my-social-network/static/css/main.b1eb563e.chunk.css"
+  },
+  {
+    "revision": "d76e847f3807c7a62ec1",
+    "url": "/my-social-network/static/js/2.ddcf5a5e.chunk.js"
+  },
+  {
+    "revision": "10226f6243d3eddac48fa465895c8df9",
+    "url": "/my-social-network/static/js/2.ddcf5a5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "918c382305574acb5d69",
+    "url": "/my-social-network/static/js/3.ed4c0e69.chunk.js"
+  },
+  {
+    "revision": "7513caea159afe168f51",
+    "url": "/my-social-network/static/js/main.609a0c49.chunk.js"
+  },
+  {
+    "revision": "6962908e45fcf0b3b394",
+    "url": "/my-social-network/static/js/runtime-main.10775585.js"
+  },
+  {
+    "revision": "cac6f4f6ddbe92403ef75aab346d1f59",
+    "url": "/my-social-network/static/media/preloader.cac6f4f6.gif"
+  },
+  {
+    "revision": "c66e5219cace6c3c4a6c9b6fc3971887",
+    "url": "/my-social-network/static/media/user.c66e5219.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-social-network/precache-manifest.59df3d69040e6597b9af7f076de42e16.js"
+  "/my-social-network/precache-manifest.62faf30388434eb9c2f80e205029de94.js"
 );
 
 self.addEventListener('message', (event) => {
